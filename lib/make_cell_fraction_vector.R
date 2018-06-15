f516a84bc4b2a1871e1de450b9f30fea00a331d2
Gregor Sturm <mail@gregor-sturm.de>
2018-06-15 12:04:39 +0000

    finish validation analysis and add noise robustness

--- lib/make_cell_fraction_vector.R
+++ lib/make_cell_fraction_vector.R
@@ -3,9 +3,9 @@
 #' It is possible to add background cells (i.e. unrelated cells)
 #' 
 #' @param input_cell_type cell_type for the immune cell to use
-#' @param n number of immune cells to use in the sample
+#' @param k number of immune cells to use in the sample
 #' @param background named vector giving cell counts of background cells. use `NULL` to exclude background cells. 
-make_cell_fraction_vector = function(input_cell_type, n, background=cell_types$background_cells) {
+make_cell_fraction_vector = function(input_cell_type, k, background=cell_types$n_background_cells) {
   if(input_cell_type == "T cell CD4+") {
     # combine regulatory and non-regulatory CD4+ T cells
     cell_type_stats = pData(single_cell_schelker$eset) %>% 
@@ -14,10 +14,10 @@ make_cell_fraction_vector = function(input_cell_type, n, background=cell_types$b
       as.data.frame() %>% 
       column_to_rownames("cell_type")
     ratio = cell_type_stats["T cell regulatory (Tregs)", "n"]/cell_type_stats["T cell CD4+ (non-regulatory)", "n"]
-    cell_n =  c(background, round(n*(1-ratio)), round(n*ratio))
+    cell_n =  c(background, round(k*(1-ratio)), round(k*ratio))
     names(cell_n) = c(names(background), "T cell CD4+ (non-regulatory)", "T cell regulatory (Tregs)")
   } else {
-    cell_n =  c(background, n)
+    cell_n =  c(background, k)
     names(cell_n) = c(names(background), input_cell_type)
   }
   cell_n

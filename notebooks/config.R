836b8a631741fb63f3f547ec4a05c7f700e1a9f9
Gregor Sturm <mail@gregor-sturm.de>
2018-11-20 10:01:47 +0000

    Limit xCell cell types to the cell types in the datasets.
    See grst/immunedeconv#1.

--- notebooks/config.R
+++ notebooks/config.R
@@ -9,7 +9,7 @@ config$cibersort_mat = "../lib/CIBERSORT/LM22.txt"
 ############## Specify methods used in the benchmark ##################
 
 # these methods will be used in the benchmark. Default: Use all methods
-# provided by the `immundeconv` package. 
+# provided by the `immundeconv` package.
 config$deconvolution_methods = immunedeconv::deconvolution_methods
 
 # these methods will be treated as 'absolute' methods in the mixing benchmark
@@ -20,8 +20,8 @@ config$abs_methods_validation = c("cibersort_abs", "quantiseq", "epic")
 
 # these methods provide scores that support within-sample comparisons
 config$methods_within_sample_comparison = c("cibersort", "cibersort_abs", "quantiseq", "epic")
-  
-  
+
+
 ############# Exclude CIBERSORT if unavailable ########################
 if(!file.exists(config$cibersort_binary) || !file.exists(config$cibersort_mat)) {
   exclude_cibersort = function(method_list) {
@@ -36,3 +36,20 @@ if(!file.exists(config$cibersort_binary) || !file.exists(config$cibersort_mat)) 
   set_cibersort_binary(config$cibersort_binary)
   set_cibersort_mat(config$cibersort_mat)
 }
+
+
+############# Expected cell types for xCell #########################
+# xCell performance can be improved by reducing the
+# number of signatures considered. (https://github.com/grst/immunedeconv/issues/1).
+# Arguably, many of the xCell signatures do not make sense in
+# the context of this benchmark, therefore we limit the signatures
+# to the cell types available in the immune datasets.
+
+EXPECTED_CELL_TYPES_SC = c("B cell", "Cancer associated fibroblast", "Dendritic cell",
+                           "Endothelial cell", "Macrophage/Monocyte", "NK cell",
+                           "T cell CD4+ (non-regulatory)", "T cell CD8+",
+                           "T cell regulatory (Tregs)")
+
+EXPECTED_CELL_TYPES_FACS = c("B cell", "Dendritic cell", "Monocyte", "NK cell",
+                           "T cell CD4+", "T cell CD8+", "T cell")
+

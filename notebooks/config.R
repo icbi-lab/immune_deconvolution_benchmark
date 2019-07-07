b9032f6e6f6206d093b334ba11d33fe067be625a
Gregor Sturm <mail@gregor-sturm.de>
2019-07-07 08:10:06 +0000

    add notebook that generates additional plots for presentation

--- notebooks/config.R
+++ notebooks/config.R
@@ -1,6 +1,6 @@
 config = new.env()
 
-registerDoMC(min(detectCores(), 16))
+registerDoMC(min(detectCores(), 2))
 
 # path to CIBERSORT script and matrix.
 config$cibersort_binary = "../lib/CIBERSORT/CIBERSORT.R"
